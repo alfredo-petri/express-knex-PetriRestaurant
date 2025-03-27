1ae14757374b49fd7ddcb9aef8306401123aba2d
alfredo-petri <alfredo_petri@hotmail.com>
2025-03-27 17:52:04 -0300

    feat: add zod errors on error handling

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,5 +1,6 @@
 import { AppError } from '@/utils/AppError'
 import { Request, Response, NextFunction } from 'express'
+import { ZodError } from 'zod'
 
 export const errorHandler = (
     error: { message: string },
@@ -11,7 +12,20 @@ export const errorHandler = (
         return response
             .status(error.statusCode)
             .json({ message: error.message })
-    } else {
-        return response.status(500).json({ message: error.message })
     }
+
+    if (error instanceof ZodError) {
+        const formattedIssues = error.errors.map((issue) => ({
+            field: issue.path[0],
+            message: issue.message,
+            code: issue.code,
+        }))
+
+        return response.status(400).json({
+            message: 'Validation error:',
+            issues: formattedIssues,
+        })
+    }
+
+    return response.status(500).json({ message: error.message })
 }
