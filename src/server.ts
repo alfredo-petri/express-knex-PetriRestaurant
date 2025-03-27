5b8d00c30462b213eed8e901a8d108fc2ebfda7e
alfredo-petri <alfredo_petri@hotmail.com>
2025-03-27 09:18:29 -0300

    feat: error handling

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { routes } from './routes'
+import { errorHandler } from './middlewares/error-handler'
 
 const PORT = 3333
 
@@ -7,6 +8,9 @@ const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+app.use(errorHandler)
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
