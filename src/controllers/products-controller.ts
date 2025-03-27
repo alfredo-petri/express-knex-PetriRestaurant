c4fb1edc3c3b6f6c2a5360970a1c5c5369920b44
alfredo-petri <alfredo_petri@hotmail.com>
2025-03-27 17:52:33 -0300

    feat: create product

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,10 +1,22 @@
+import { knex } from '@/database/knex'
+import { productSchema } from '@/schemas/products/product-schema'
 import { NextFunction, Request, Response } from 'express'
-
 class ProductsController {
     async index(request: Request, response: Response, next: NextFunction) {
         try {
-            console.log(request, response)
             return response.json({ message: 'ok' })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async create(request: Request, response: Response, next: NextFunction) {
+        try {
+            const { name, price } = productSchema.parse(request.body)
+
+            await knex<ProductsTable>('products').insert({ name, price })
+
+            return response.status(201).json()
         } catch (error) {
             next(error)
         }
