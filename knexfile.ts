29a70b9de8a32f1c2731d723b33d4c924308ea57
alfredo-petri <alfredo_petri@hotmail.com>
2025-03-27 10:24:06 -0300

    chore: configure knex

---
+++ knexfile.ts
@@ -0,0 +1,24 @@
+import { Knex } from 'knex'
+
+export default {
+    client: 'sqlite3',
+    connection: {
+        filename: './db/app.db',
+    },
+    pool: {
+        // eslint-disable-next-line
+        afterCreate: (connection: any, done: any) => {
+            connection.run('PRAGMA foreign_keys = ON')
+            done()
+        },
+    },
+    useNullAsDefault: true,
+    migrations: {
+        extension: 'ts',
+        directory: './src/database/migrations',
+    },
+    seeds: {
+        extension: 'ts',
+        directory: './src/database/seeds',
+    },
+} as Knex.Config
