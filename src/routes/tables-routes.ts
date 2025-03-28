f01ef1f9295db78d5fda89f7e967d39cd7e0dd43
alfredo-petri <alfredo_petri@hotmail.com>
2025-03-28 09:58:57 -0300

    feat: create tables routes

---
+++ src/routes/tables-routes.ts
@@ -0,0 +1,10 @@
+import { TablesController } from '@/controllers/tables-controller'
+import { Router } from 'express'
+
+const tablesRoutes = Router()
+
+const tablesController = new TablesController()
+
+tablesRoutes.get('/', tablesController.list)
+
+export { tablesRoutes }
