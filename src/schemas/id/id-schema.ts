ac6fd476e6c09a6f37ed1bd213a9d332bc5bd214
alfredo-petri <alfredo_petri@hotmail.com>
2025-03-28 13:59:39 -0300

    perf: update id schema validations

--- src/schemas/id/id-schema.ts
+++ src/schemas/id/id-schema.ts
@@ -5,6 +5,12 @@ export const idParamSchema = z
     .transform((value) => Number(value))
     .refine((value) => !isNaN(value), { message: 'id must be a number' })
 
+export const idQueryParamSchema = z
+    .string()
+    .transform((value) => Number(value))
+    .refine((value) => !isNaN(value), { message: 'id must be a number' })
+    .optional()
+
 export const idBodySchema = z.number({
     required_error: 'id is required',
     invalid_type_error: 'id must be a number',
