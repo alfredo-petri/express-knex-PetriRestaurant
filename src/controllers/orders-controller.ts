92c29287140c3705d577f222adb041fe7f7c7eb5
alfredo-petri <alfredo_petri@hotmail.com>
2025-03-29 18:30:05 -0300

    feat: list controller

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -1,9 +1,103 @@
 import { knex } from '@/database/knex'
+import { idQueryParamSchema } from '@/schemas/id/id-schema'
 import { createOrderSchema } from '@/schemas/orders/create-order-schema'
 import { AppError } from '@/utils/AppError'
 import { Request, Response, NextFunction } from 'express'
 
 class OrdersController {
+    async list(request: Request, response: Response, next: NextFunction) {
+        try {
+            const tableSessionId = idQueryParamSchema.parse(
+                request.query.table_session_id,
+            )
+
+            // todo: incluir table number na resposta
+            // const tableSession = await knex<TTablesSessions>('tables_sessions')
+            //     .select('*')
+            //     .where({ id: tableSessionId })
+            //     .first()
+            // let tableNumber: number | undefined
+            // if (tableSession) {
+            //     const tableNumberQuery = await knex<TTables>('tables')
+            //         .select('table_number')
+            //         .where({ id: tableSession.table_id })
+            //         .first()
+            //     tableNumber = tableNumberQuery?.table_number
+            // }
+
+            let query = knex<TOrders>('orders')
+                .select(
+                    'orders.table_session_id',
+                    'products.name',
+                    'orders.quantity',
+                    'orders.price',
+                    knex.raw('(orders.quantity * orders.price) AS total'),
+                    'orders.updated_at',
+                    'tables_sessions.opened_at',
+                    'tables_sessions.closed_at',
+                )
+                .join('products', 'products.id', 'orders.product_id')
+                .join(
+                    'tables_sessions',
+                    'tables_sessions.id',
+                    'orders.table_session_id',
+                )
+                .orderBy('orders.updated_at', 'desc')
+
+            if (tableSessionId) {
+                query = query.where({ table_session_id: tableSessionId })
+            }
+            const orders = await query
+
+            if (!orders.length) {
+                throw new AppError(
+                    'there are no products added to this table session',
+                    404,
+                )
+            }
+
+            const groupedOrders = orders.reduce((acc, order) => {
+                const {
+                    table_session_id: tableSessionId,
+                    total,
+                    opened_at: openedAt,
+                    closed_at: closedAt,
+                    ...orderData
+                } = order
+
+                if (!acc[tableSessionId]) {
+                    acc[tableSessionId] = {
+                        table_session_id: tableSessionId,
+                        orders: [],
+                        total_amount: 0,
+                        opened_at: openedAt,
+                        closed_at: null,
+                    }
+                }
+
+                acc[tableSessionId].orders.push(orderData)
+                acc[tableSessionId].total_amount += total
+
+                acc[tableSessionId].opened_at =
+                    acc[tableSessionId].opened_at < openedAt
+                        ? acc[tableSessionId].opened_at
+                        : openedAt
+
+                if (closedAt !== null) {
+                    acc[tableSessionId].closed_at = closedAt
+                }
+
+                return acc
+            }, {})
+
+            const orderSummary = Object.values(groupedOrders)
+
+            return response.status(200).json({ order_summary: orderSummary })
+        } catch (error) {
+            next(error)
+        }
+    }
+
     async create(request: Request, response: Response, next: NextFunction) {
         try {
             const {
--- src/routes/orders-routes.ts
+++ src/routes/orders-routes.ts
@@ -5,6 +5,7 @@ const ordersRoutes = Router()
 
 const ordersController = new OrdersController()
 
+ordersRoutes.get('/', ordersController.list)
 ordersRoutes.post('/', ordersController.create)
 
 export { ordersRoutes }
