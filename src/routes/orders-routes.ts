a25883597fc412f5342bd08ff7c781352d50b88a
alfredo-petri <alfredo_petri@hotmail.com>
2025-03-29 07:48:37 -0300

    feat: create orders routes

---
+++ src/routes/orders-routes.ts
@@ -0,0 +1,10 @@
+import { OrdersController } from '@/controllers/orders-controller'
+import { Router } from 'express'
+
+const ordersRoutes = Router()
+
+const ordersController = new OrdersController()
+
+ordersRoutes.post('/', ordersController.create)
+
+export { ordersRoutes }
