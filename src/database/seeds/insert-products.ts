47d516c9e03703573746176060dd7e35bf545ccd
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-30 07:32:25 -0300

    feat: update seeds

--- src/database/seeds/insert-products.ts
+++ src/database/seeds/insert-products.ts
@@ -4,11 +4,15 @@ export async function seed(knex: Knex): Promise<void> {
     await knex('products').del()
 
     await knex('products').insert([
-        { name: 'x-salada', price: 16 },
-        { name: 'x-egg', price: 18 },
-        { name: 'x-egg-bacon', price: 20 },
-        { name: 'x-frango', price: 19.5 },
-        { name: 'x-calabresa', price: 19 },
-        { name: 'x-tudo', price: 24 },
+        { name: 'Spaghetti Carbonara', price: 18 },
+        { name: 'Lasagna', price: 22 },
+        { name: 'Pizza Margherita', price: 15 },
+        { name: 'Risotto alla Milanese', price: 24 },
+        { name: 'Fettuccine Alfredo', price: 20 },
+        { name: 'Osso Buco', price: 30 },
+        { name: 'Tiramisu', price: 10 },
+        { name: 'Gnocchi al Pesto', price: 18 },
+        { name: 'Ravioli di Ricotta', price: 21 },
+        { name: 'Bruschetta', price: 12 },
     ])
 }
