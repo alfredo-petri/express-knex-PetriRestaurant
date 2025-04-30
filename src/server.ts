c300e795ed97ec1d05950259dd805740d24807d3
alfredo-petri <alfredo_petri@hotmail.com>
2025-04-30 06:34:37 -0300

    feat: add api documentation

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
 import express from 'express'
 import { routes } from './routes'
 import { errorHandler } from './middlewares/error-handler'
+import swaggerUi from 'swagger-ui-express'
+import YAML from 'yamljs'
 
 const PORT = 3333
 
 const app = express()
 
+const swaggerDocument = YAML.load('./docs/openapi.yaml')
+
 app.use(express.json())
 app.use(routes)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
+    console.log(`📚 Swagger docs available at http://localhost:${PORT}/docs`)
 })
